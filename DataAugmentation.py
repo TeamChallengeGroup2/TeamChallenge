41a32c9e420e3abe5b431afaa8ec0e780b62ef6a
DieterTimmers <46112022+DieterTimmers@users.noreply.github.com>
2019-04-02 14:27:40 +0200

    Add files via upload

--- DataAugmentation.py
+++ DataAugmentation.py
@@ -18,50 +18,19 @@ import matplotlib.pyplot as plt
 
 
 
-def create_Augmented_Data(data_original):
-    # CROPPING
-    cropped_data=[] # List with the patient number, the slices number, and the four 2D slices as arrays
-    slice_count=[]
-    
-    for j in range(len(data_original)):
-    
-        # Extract the ED frame and ES frame for each patient, separately
-        EDframe=data_original[j][2]
-        ESframe=data_original[j][4]
-        
-        # Crop only if HoughCircles is able to find a circle
-        cropped_EDim, EDx1, EDx2, EDy1, EDy2=cropROI(EDframe,4)
-        cropped_ESim, ESx1, ESx2, ESy1, ESy2=cropROI(ESframe,4)
-        if cropped_EDim.size and cropped_ESim.size:
-            # Extract the slice number
-            n=data_original[j][1]
-            slice_count.append(n)
-            # Extract and save the ED and ES slices and ground truth slices
-            for h in range(n):
-                EDslice=data_original[j][2][h]
-                EDslicegt=data_original[j][3][h]
-                ESslice=data_original[j][4][h]
-                ESslicegt=data_original[j][5][h]
-                
-                # Save the data in lists
-                cropped_data.append([data_original[j][0],h+1,EDslice[EDx1:EDx2, EDy1:EDy2],EDslicegt[EDx1:EDx2, EDy1:EDy2],ESslice[ESx1:ESx2, ESy1:ESy2],ESslicegt[ESx1:ESx2, ESy1:ESy2],data_original[j][6]])
-                
-                
-    print('Images cropped')
-    
-    
-    size_array=2*len(cropped_data)
-    images=np.zeros((size_array,126,126))
-    segmentations=np.zeros((size_array,126,126))
-    j=0
-    for i in range(size_array):
-        if i<len(cropped_data):
-            images[i,:,:]=cropped_data[i][2]
-            segmentations[i,:,:]=cropped_data[i][3]
-        else:
-            images[i,:,:]=cropped_data[j][4]
-            segmentations[i,:,:]=cropped_data[j][5]
-            j+=1
+def create_Augmented_Data(images, segmentations, b_size, numberofbatches):
+#    size_array=2*len(cropped_data)
+#    images=np.zeros((size_array,126,126))
+#    segmentations=np.zeros((size_array,126,126))
+#    j=0
+#    for i in range(size_array):
+#        if i<len(cropped_data):
+#            images[i,:,:]=cropped_data[i][2]
+#            segmentations[i,:,:]=cropped_data[i][3]
+#        else:
+#            images[i,:,:]=cropped_data[j][4]
+#            segmentations[i,:,:]=cropped_data[j][5]
+#            j+=1
         
  
        
@@ -92,36 +61,32 @@ def create_Augmented_Data(data_original):
 
     print('Generate Images')
     batches=0
-    for x_batch in image_datagen.flow(images, batch_size=25, seed=seed):
+    for x_batch in image_datagen.flow(images, batch_size=b_size, seed=seed):
         print('Batch: ', batches)
         x_batch=np.squeeze(x_batch)
         augmented_images=np.concatenate((augmented_images,x_batch), axis=0)
         batches+=1
-        if batches >=3:
+        if batches >= numberofbatches:
             break
         
        
     print('Generate Segmentations')
     batches=0
-    for y_batch in image_datagen.flow(segmentations, batch_size=25, seed=seed):
+    for y_batch in image_datagen.flow(segmentations, batch_size=b_size, seed=seed):
         print('Batch: ', batches)
         y_batch=np.squeeze(y_batch)
         augmented_segmentations=np.concatenate((augmented_segmentations,y_batch), axis=0)
         batches+=1
-        if batches >=3:
+        if batches >= numberofbatches:
             break
        
     return augmented_images, augmented_segmentations
 
-path=r'C:\Users\s141352\Documents\BMT\Master\Team Challenge\Part 2'
-data_original=loadData(path)
-augmented_images, augmented_segmentations=create_Augmented_Data(data_original)
-
-
-plt.figure()
-plt.imshow(augmented_images[1802,:,:], cmap='gray')
-plt.show()
-
-plt.figure()
-plt.imshow(augmented_segmentations[1802,:,:], cmap='gray')
-plt.show()
+#
+#plt.figure()
+#plt.imshow(augmented_images[1802,:,:], cmap='gray')
+#plt.show()
+#
+#plt.figure()
+#plt.imshow(augmented_segmentations[1802,:,:], cmap='gray')
+#plt.show()
--- Main.py
+++ Main.py
@@ -11,23 +11,27 @@ import time
 import random
 import os
 import cv2
+import matplotlib.pyplot as plt
 
 from Data import loadData
-from Augmentation import augmentation
 from Cropping import cropROI
 from fcnNetwork import fcn_model
 from Validate import plotResults, metrics
+from DataAugmentation import create_Augmented_Data
 
 # -----------------------------------------------------------------------------
 # INPUT
-path = os.path.realpath("Main.py").replace("\\Main.py","")
+path = r'C:\Users\s141352\Documents\BMT\Master\Team Challenge\Part 2'
 networkpath = r'trainednetwork.h5'
-nr_augmentations = 30
+nr_of_batches_augmentation = 30
+batchsize_augmentation = 25
 batchsize = 5
 epochs = 10
 trainnetwork = True
 testing = True
 plot = False
+augmentation = True
+EjectionFraction = False
 
 # -----------------------------------------------------------------------------
 # LOADING THE DATA
@@ -35,37 +39,33 @@ data_original=loadData(path)
 print('Data Loaded')
 
 # -----------------------------------------------------------------------------
-# DATA AUGMENTATION
-data=augmentation(data_original,nr_augmentations)
-print('Augmentation succeeded')
-
-# -----------------------------------------------------------------------------
 # CROPPING
-output=[] # List with the patient number, the slices number, and the four 2D slices as arrays
-slice_count=[0]
+data=[] # List with the patient number, the slices number, and the four 2D slices as arrays
+slice_count=[]
 
-for j in range(len(data)):
+for j in range(len(data_original)):
 
     # Extract the ED frame and ES frame for each patient, separately
-    EDframe=data[j][2]
-    ESframe=data[j][4]
+    EDframe=data_original[j][2]
+    ESframe=data_original[j][4]
     
     # Crop only if HoughCircles is able to find a circle
     cropped_EDim, EDx1, EDx2, EDy1, EDy2=cropROI(EDframe,4)
     cropped_ESim, ESx1, ESx2, ESy1, ESy2=cropROI(ESframe,4)
     if cropped_EDim.size and cropped_ESim.size:
         # Extract the slice number
-        n=data[j][1]
+        n=data_original[j][1]
+        slice_count.append(n)
         # Extract and save the ED and ES slices and ground truth slices
         for h in range(n):
-            EDslice=data[j][2][h]
-            EDslicegt=data[j][3][h]
-            ESslice=data[j][4][h]
-            ESslicegt=data[j][5][h]
+            EDslice=data_original[j][2][h]
+            EDslicegt=data_original[j][3][h]
+            ESslice=data_original[j][4][h]
+            ESslicegt=data_original[j][5][h]
             
             # Save the data in lists
-            output.append([data[j][0],h+1,EDslice[EDx1:EDx2, EDy1:EDy2],EDslicegt[EDx1:EDx2, EDy1:EDy2],ESslice[ESx1:ESx2, ESy1:ESy2],ESslicegt[ESx1:ESx2, ESy1:ESy2],data[j][6]])
-            slice_count.append(slice_count[j]+n)
+            data.append([data_original[j][0],h+1,EDslice[EDx1:EDx2, EDy1:EDy2],EDslicegt[EDx1:EDx2, EDy1:EDy2],ESslice[ESx1:ESx2, ESy1:ESy2],ESslicegt[ESx1:ESx2, ESy1:ESy2],data_original[j][6]])
+            
             
 print('Images cropped')
 
@@ -73,46 +73,60 @@ print('Images cropped')
 # TRAINING
 
 # Shuffle the data to take a random subset for training later
-random.shuffle(output)
-
-# Split the list l into a list containing all ED frames
-frames = []
-groundtruth = []
-spacings = []
-
-for i in range(len(output)):    
-    # Append the ED frame 
-    frames.append(output[i][2])
-    # Append the ES frame
-    frames.append(output[i][4])
-    # Append the ED groundtruth 
-    groundtruth.append(output[i][3])
-    # Append the ES groundtruth
-    groundtruth.append(output[i][5])
-    # Append the spacing
-    spacings.append(output[i][6])
-
-# Convert the lists containing the frames and groundtruth to arrays
-frames=np.array(frames)
-groundtruth=np.array(groundtruth)
+random.shuffle(data)
 
 # Split up the data set into a training and test set. Note that the validation frames
 # are included in the Train_frames array
-Train_frames = frames[:3*int(len(frames)/4)]
-Test_frames = frames[int(len(frames)-len(frames)/4):]
+Train_frames = data[:3*int(len(data)/4)]
+Test_frames = data[int(len(data)-len(data)/4):]
 
-Train_frames = np.array(Train_frames)
-Test_frames = np.array(Test_frames)
 
-Train_labels = groundtruth[:3*int(len(groundtruth)/4)]
-Test_labels = groundtruth[int(len(groundtruth)-len(groundtruth)/4):]
+# Split the list l into a list containing all ED frames
+framesTrain = []
+groundtruthTrain = []
+spacingsTrain = []
 
-Train_labels = np.array(Train_labels)
-Test_labels = np.array(Test_labels)
+framesTest = []
+groundtruthTest = []
+spacingsTest = []
+
+for i in range(len(Train_frames)):    
+    # Append the ED frame 
+    framesTrain.append(Train_frames[i][2])
+    # Append the ES frame
+    framesTrain.append(Train_frames[i][4])
+    # Append the ED groundtruth 
+    groundtruthTrain.append(Train_frames[i][3])
+    # Append the ES groundtruth
+    groundtruthTrain.append(Train_frames[i][5])
+    # Append the spacing
+    spacingsTrain.append(Train_frames[i][6])
+    
+for i in range(len(Test_frames)):    
+    # Append the ED frame 
+    framesTest.append(Test_frames[i][2])
+    # Append the ES frame
+    framesTest.append(Test_frames[i][4])
+    # Append the ED groundtruth 
+    groundtruthTest.append(Test_frames[i][3])
+    # Append the ES groundtruth
+    groundtruthTest.append(Test_frames[i][5])
+    # Append the spacing
+    spacingsTest.append(Test_frames[i][6])
+
+framesTrain = np.array(framesTrain)
+framesTest = np.array(framesTest)
+
+groundtruthTrain = np.array(groundtruthTrain)
+groundtruthTest = np.array(groundtruthTest)
 
 # Scale the masks to binary masks
-Train_labels = cv2.threshold(Train_labels,2.5,1.0,cv2.THRESH_BINARY)[1]
-Test_labels = cv2.threshold(Test_labels,2.5,1.0,cv2.THRESH_BINARY)[1]
+groundtruthTrain = cv2.threshold(groundtruthTrain,2.5,1.0,cv2.THRESH_BINARY)[1]
+groundtruthTest = cv2.threshold(groundtruthTest,2.5,1.0,cv2.THRESH_BINARY)[1]
+
+# Augment the data
+if augmentation:
+    framesTrain, groundtruthTrain = create_Augmented_Data(framesTrain, groundtruthTrain, batchsize_augmentation, nr_of_batches_augmentation)
 
 # Initialize the model
 cnn  = fcn_model((126,126,1),2,weights=None)
@@ -124,7 +138,7 @@ if trainnetwork:
     t0 = time.time()
     
     # Train the network 
-    hist=cnn.fit(Train_frames[:,:,:,np.newaxis], Train_labels[:,:,:,np.newaxis],
+    hist=cnn.fit(framesTrain[:,:,:,np.newaxis], groundtruthTrain[:,:,:,np.newaxis],
             batch_size=batchsize, epochs=epochs, verbose=1, shuffle=True, validation_split = 0.25)
     
     # Save the network
@@ -145,47 +159,49 @@ print ('Start testing')
 if testing:
     
     # Predict the masks
-    mask = cnn.predict(Test_frames[:,:,:,np.newaxis], verbose=1)
+    mask = cnn.predict(framesTest[:,:,:,np.newaxis], verbose=1)
 
     # As the prediction have the channels dimension (3th dimension per slice), 
     # to go back to 2 dimensions per slice:
     mask=np.squeeze(mask)
     
     # Compute the DICE coefficient, accuracy, sensitivity and specificity per image
-    Dice, Accuracy, Sensitivity, Specificity = metrics(mask, Test_labels)
+    Dice, Accuracy, Sensitivity, Specificity = metrics(mask, groundtruthTest)
     
     # Plot the results
     if plot:
-        plotResults(Test_frames, Test_labels, mask)
+        plotResults(framesTest, groundtruthTest, mask)
 
 print ('Testing is finished')
     
 # -----------------------------------------------------------------------------
 # EJECTION FRACTION
-print ('Start computing the Ejection Fraction')
 
-EF=[]
-EF_gt=[]
-
-for k in range(0,len(mask),2):
-    # Determine the voxelvolume
-    voxelvolume_ED = spacings[k][0]*spacings[k][1]*spacings[k][2]
-    voxelvolume_ES = spacings[k+1][0]*spacings[k+1][1]*spacings[k+1][2]
+if EjectionFraction:
+    print ('Start computing the Ejection Fraction')
     
-    # Compute the stroke volume from the end-diastolic (ED) and end-systolic (ES) volume
-    ED_volume = np.sum(mask[k,:,:]==1)*voxelvolume_ED
-    ES_volume = np.sum(mask[k+1,:,:]==1)*voxelvolume_ES
-    strokevolume = ED_volume - ES_volume
+    EF=[]
+    EF_gt=[]
     
-    # Compute the Ejection fraction per patient and save in a list
-    LV_EF = (strokevolume/ED_volume)*100
-    EF.append(LV_EF)
-    
-    # Ground truth
-    ED_volume_gt = np.sum(Test_labels[k,:,:]==3)*voxelvolume_ED
-    ES_volume_gt = np.sum(Test_labels[k+1,:,:]==3)*voxelvolume_ES
-    strokevolume_gt = ED_volume_gt - ES_volume_gt
-    LV_EF_gt = (strokevolume_gt/ED_volume_gt)*100
-    EF_gt.append(LV_EF_gt)
-    
-print ('Ejection Fraction is computed')
+    for k in range(0,len(mask),2):
+        # Determine the voxelvolume
+        voxelvolume_ED = spacings[k][0]*spacings[k][1]*spacings[k][2]
+        voxelvolume_ES = spacings[k+1][0]*spacings[k+1][1]*spacings[k+1][2]
+        
+        # Compute the stroke volume from the end-diastolic (ED) and end-systolic (ES) volume
+        ED_volume = np.sum(mask[k,:,:]==1)*voxelvolume_ED
+        ES_volume = np.sum(mask[k+1,:,:]==1)*voxelvolume_ES
+        strokevolume = ED_volume - ES_volume
+        
+        # Compute the Ejection fraction per patient and save in a list
+        LV_EF = (strokevolume/ED_volume)*100
+        EF.append(LV_EF)
+        
+        # Ground truth
+        ED_volume_gt = np.sum(Test_labels[k,:,:]==3)*voxelvolume_ED
+        ES_volume_gt = np.sum(Test_labels[k+1,:,:]==3)*voxelvolume_ES
+        strokevolume_gt = ED_volume_gt - ES_volume_gt
+        LV_EF_gt = (strokevolume_gt/ED_volume_gt)*100
+        EF_gt.append(LV_EF_gt)
+        
+    print ('Ejection Fraction is computed')
