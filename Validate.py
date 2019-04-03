cfabfbe4a0e378ec3d62c97760af56021da02401
Vera07 <46816278+Vera07@users.noreply.github.com>
2019-04-03 09:19:37 +0200

    Add files via upload

--- Validate.py
+++ Validate.py
@@ -6,72 +6,57 @@ Team 2
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Function to plot some validation results
-def plotResults(Valid_frames, Valid_labels, mask):
+def plotResults(Test_frames, Test_labels, mask):
     
     plt.figure()
-    for i in range(1, len(Valid_frames), 3):
-        plt.subplot(len(Valid_frames), 3, i)
-        plt.imshow(Valid_frames[i-1], cmap='gray')
-        plt.subplot(len(Valid_frames), 3, i+1)
-        plt.imshow(Valid_frames[i], cmap='gray')
-        plt.subplot(len(Valid_frames), 3, i+2)
-        plt.imshow(Valid_frames[i+1], cmap='gray')
+    for i in range(1, len(Test_frames), 3):
+        plt.subplot(len(Test_frames), 3, i)
+        plt.imshow(Test_frames[i-1], cmap='gray')
+        plt.subplot(len(Test_frames), 3, i+1)
+        plt.imshow(Test_frames[i], cmap='gray')
+        plt.subplot(len(Test_frames), 3, i+2)
+        plt.imshow(Test_frames[i+1], cmap='gray')
 
-def metrics(mask,Valid_labels):
+def metrics(mask, Test_labels):
     Dice = []
     Accuracy = []
     Sensitivity = []
     Specificity = []
-
-    # Compute the True positives (TP), False negatives (FN), False positives (FP) and True negatives (TN)
+    
     for i in range(mask.shape[0]):
-        TP = 0
-        FP = 0
-        TN = 0
-        FN = 0
-        for j in range(mask.shape[1]):
-            for k in range(mask.shape[2]):
-                if mask[i,j,k] == 1 and Valid_labels[i,j,k] == 3:
-                    TP = TP+1
-                    
-                if mask[i,j,k] == 0 and Valid_labels[i,j,k] == 3:
-                    FN = FN+1
-                       
-                if mask[i,j,k] == 1 and Valid_labels[i,j,k] != 3:
-                    FP = FP+1
-                            
-                if mask[i,j,k] == 0 and Valid_labels[i,j,k] != 3:
-                    TN = TN+1
-        
+        TP = np.sum(mask[i,:,:] * Test_labels[i,:,:])
+        FN = np.sum(Test_labels[i,:,:]) - TP # P = TP + FN
+        FP = len(np.where(mask[i,:,:] - Test_labels[i,:,:] == 1)[0])
+        TN = len(np.where(mask[i,:,:] + Test_labels[i,:,:] == 0)[0])
+    
         # Overcome computation problems (dividing by zero) by setting the number
         # of true positives and true negatives to a very small number if these 
         # numbers are originally equal to zero
-        if TP==0:
-            TP=0.0001
+        if TP == 0:
+            TP = 0.0001
         if TN == 0:
             TN = 0.0001
             
         # Dice score
-        DSC=2*TP/(2*TP+FP+FN)   
-        if TP == 0.0001:
-            DSC = 0
+        DSC = 2*TP / (2*TP+FP+FN) 
+        DSC = round(DSC, 4)
         Dice.append(DSC)  
           
         # Accuracy
-        Acc = (TP+TN)/(TP+FN+FP+TN)
+        Acc = (TP+TN) / (TP+FN+FP+TN)
+        Acc = round(Acc, 4)
         Accuracy.append(Acc)
 
         # Sensitivity and Specificity
-        Sens = TP/(TP+FN)
-        if TP == 0.0001:
-            Sens = 0
+        Sens = TP / (TP+FN)
+        Sens = round(Sens, 4)
         Sensitivity.append(Sens)
 
-        Spec = TN/(FP+TN)
-        if TN == 0.0001:
-            Spec = 0
+        Spec = TN / (FP+TN)
+        Spec = round(Spec, 4)
         Specificity.append(Spec)
         
     return Dice, Accuracy, Sensitivity, Specificity
